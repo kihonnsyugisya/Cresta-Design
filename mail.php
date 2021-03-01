af949c1168184d884c38cbd2c20c8a8ce3a9385f
田辺 <katuobrothers@icloud.com>
2021-03-01 20:38:54 +0900

    アドレス変更

--- mail.php
+++ mail.php
@@ -30,7 +30,7 @@ if (version_compare(PHP_VERSION, '5.1.0', '>=')) {//PHP5.1.0以上の場合の�
 //---------------------------　必須設定　必ず設定してください　-----------------------
 
 //サイトのトップページのURL　※デフォルトでは送信完了後に「トップページへ戻る」ボタンが表示されますので
-$site_top = "./index.html";
+$site_top = "https://kihonnsyugisya.github.io/Cresta-Design/";
 
 //管理者のメールアドレス ※メールを受け取るメールアドレス(複数指定する場合は「,」で区切ってください 例 $to = "aa@aa.aa,bb@bb.bb";)
 $to = "info@kihonn-syugisya.com";
